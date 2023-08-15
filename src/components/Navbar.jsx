0328eaa8b98d6010fe909e3144e4ef74202f5d46
Ahmed Azim <ahmed.azim117@gmail.com>
2023-08-15 11:02:38 +1000

    materialui

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,6 +5,9 @@ const Navbar = () => {
   return (
     <div className='flex flex-row justify-center w-full'>
       <img className='w-64 py-6' src={logo} alt='Chainsactions'></img>
+      <ul>
+        <li>Sign Up</li>
+      </ul>
     </div>
   )
 }
